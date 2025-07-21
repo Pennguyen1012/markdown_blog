0fd18b3bdc225a0f0bb8ebada0bbd6c624e7ea42
Pennguyen1012 <pennguyen31@gmail.com>
2025-07-22 02:14:42 +0700

    added the connection to mongodb, coded the logic for creating a new article

--- routes/articles.js
+++ routes/articles.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const router = express()
 
+//import the table from models/articles.js
+const Article = require('./../models/articles')
+
+//this is for routing to new article
 router.get('/new', (req, res) => {
     res.render('articles/new')
 })
 
-router.post('/', (req, res) => {
+//this is for routing to the newly created article
+router.get('/:id', (req, res) => {
 
+})
+
+router.post('/', async (req, res) => {
+    const article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    try {
+        article = await articles.save()
+        res.redirect(`/articles/${articles.id}`)
+    } catch (error) {
+        res.render('articles/new', {article: article})
+        console.log(error)
+    }
 })
 
 //export the router
 module.exports = router
+
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articlesRouter = require('./routes/articles')
 const app = express()
+
+
+mongoose.connect('mongodb://localhost/blog', {
+})
+
+console.log('Connected to Mongoose')
 
 //setup: using ejs and use view engine to convert to html
 app.set('view engine', 'ejs')
     //note: this ref to the views folder with ejs files
 
 //use routers
+app.use(express.urlencoded({extended: false}))
 app.use('/articles', articlesRouter)
 
 //actual content
@@ -24,6 +32,8 @@ app.get('/', (req, res) => {
     }]
     res.render('articles/index', {articles: articles})
 })
+
+
 
 //this to really push the app up to port 5000
 app.listen(5000)
