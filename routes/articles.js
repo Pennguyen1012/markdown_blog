95a056dd2e8280e400d9d74773fdda5196a3c150
Pennguyen1012 <pennguyen31@gmail.com>
2025-07-21 17:59:46 +0700

    the code, created the blog part with the articles

---
+++ routes/articles.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express()
+
+router.get('/new', (req, res) => {
+    res.render('articles/new')
+})
+
+router.post('/', (req, res) => {
+
+})
+
+//export the router
+module.exports = router
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const articlesRouter = require('./routes/articles')
+const app = express()
+
+//setup: using ejs and use view engine to convert to html
+app.set('view engine', 'ejs')
+    //note: this ref to the views folder with ejs files
+
+//use routers
+app.use('/articles', articlesRouter)
+
+//actual content
+app.get('/', (req, res) => {
+
+    const articles = [{
+        title: "Text articles",
+        createdAt: new Date(),
+        description: "Some descriptions"
+    },
+    {
+        title: "Text articles 2",
+        createdAt: new Date(),
+        description: "Some descriptions"
+    }]
+    res.render('articles/index', {articles: articles})
+})
+
+//this to really push the app up to port 5000
+app.listen(5000)
